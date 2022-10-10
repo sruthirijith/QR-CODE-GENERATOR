c7b8ce826a9159f9bcd637d8dcafeb5e204f066d
sruthirijith <97118800+sruthirijith@users.noreply.github.com>
2022-10-10 12:46:52 +0530

    Add files via upload

---
+++ codecolor.py
@@ -0,0 +1,13 @@
+from textwrap import fill
+from turtle import back, fillcolor
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(version=1, 
+                  error_correction=qrcode.constants.ERROR_CORRECT_H,
+                  box_size=10,border=4)
+qr.add_data("https://web.whatsapp.com")
+qr.make(fit=True)
+img = qr.make_image(fill_color="red", back_color="blue")
+img.save("colorcode.png")                  
+
---
+++ whatsapp.py
@@ -0,0 +1,3 @@
+import qrcode
+img = qrcode.make("https://web.whatsapp.com")
+img.save("sruthi_whatsapp.png")
